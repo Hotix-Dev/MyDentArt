d3c7baddc5717c71ff0da115613b08101d9c02cc
Hotix-Dev <hotixnewgeneration@gmail.com>
2023-12-28 10:56:54 +0100

    Initial commit

---
+++ app/src/main/java/com/e2p/mydentart/activites/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.e2p.mydentart.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.e2p.mydentart.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
